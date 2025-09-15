30da23cfc0567159056ac6bc7042adeb8ab4112a
rajpratham1 <188737331+rajpratham1@users.noreply.github.com>
2025-09-15 17:47:58 +0530

    chore: Add console.log to debug env vars

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+console.log(import.meta.env);
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics"; // Added this import
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
