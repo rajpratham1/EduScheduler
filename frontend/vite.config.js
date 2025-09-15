6f8c26193d5af2452b78f6f170234d76c2db6d1f
rajpratham1 <188737331+rajpratham1@users.noreply.github.com>
2025-09-15 17:44:46 +0530

    fix: Correct vite.config.js to resolve environment variable loading issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'; // Import path module
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Set up an alias for the 'src' directory
-      'src': path.resolve(__dirname, './src'),
-    },
-  },
 })
